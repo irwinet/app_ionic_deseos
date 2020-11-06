679d0715e3b18a77731ba72413d87beacbaef3f6
irwinet <irwinestradatorres@gmail.com>
2020-11-06 16:19:33 -0500

    Add Page Agregar Refactor Tab1 And Tab2 - Add LocalStorage

---
+++ src/app/pages/agregar/agregar.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { ActivatedRoute } from '@angular/router';
+import { Lista } from '../../models/lista.model';
+import { ListaItem } from '../../models/lista-item.modelo';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.page.html',
+  styleUrls: ['./agregar.page.scss'],
+})
+export class AgregarPage implements OnInit {
+
+  lista: Lista;
+  nombreItem = '';
+
+  constructor(private deseosService: DeseosService,
+              private route: ActivatedRoute) { 
+    const listaId = this.route.snapshot.paramMap.get('listaId');
+    console.log(listaId);
+
+    this.lista = deseosService.obtenerLista(listaId);
+
+    console.log(this.lista);
+  }
+
+  ngOnInit() {
+  }
+
+  agregarItem(){
+    if(this.nombreItem.length === 0){
+      return;
+    }
+
+    const nuevoItem = new ListaItem(this.nombreItem);
+
+    this.lista.items.push(nuevoItem);
+
+    this.nombreItem = '';
+    this.deseosService.guardarStorage();
+  }
+
+}
--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from 'src/app/models/lista.model';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +11,49 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public deseosService: DeseosService,
+              private router: Router,
+              private alertController: AlertController) {
+    
+  }
+
+  async agregarLista(){
+    //this.router.navigateByUrl('/tabs/tab1/agregar');
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Nueva lista',
+      inputs: [
+        {
+          name: 'titulo',
+          type: 'text',
+          placeholder: 'Nombre de la lista'
+        }
+      ],   
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancelar');
+          }
+        },
+        {
+          text: 'Crear',
+          handler: (data) => {
+            // console.log(data);
+            if (data.titulo.length === 0)
+              return;
+
+            // Crear Lista
+            const listaId = this.deseosService.crearLista(data.titulo);
+
+            this.router.navigateByUrl(`/tabs/tab1/agregar/${listaId}`);
+          }
+        }
+      ]
+    });
+
+    alert.present();
+  }
 
 }
